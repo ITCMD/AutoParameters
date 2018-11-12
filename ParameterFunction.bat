0dfbdbe8586f9c8aaf19e1291e996e6cdf992e9a
Lucas Elliott <32961763+ITCMD@users.noreply.github.com>
2018-11-12 13:00:43 -0500

    Create ParameterFunction.bat

---
+++ ParameterFunction.bat
@@ -0,0 +1,10 @@
+:ParameterFunction
+set ParamCount=0
+:FunctionLoop
+if "%~1"=="" goto EndFunction
+set /a ParamCount+=1
+set F%ParamCount%=%1
+Set F%ParamCount%Q=%~1
+shift
+goto Functionloop
+:EndFunction
